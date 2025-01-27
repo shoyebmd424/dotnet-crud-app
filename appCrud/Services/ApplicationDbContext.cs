3f7011718378f4ece97c2c3b46f727af5497a67d
Md Shoyeb <96253133+shoyebmd424@users.noreply.github.com>
2025-01-27 17:41:00 +0530

    initial commit

---
+++ appCrud/Services/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using appCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace appCrud.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+            public DbSet<Client> Clients { get; set; }
+    }
+}
