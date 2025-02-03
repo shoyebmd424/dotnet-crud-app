125ec4ec4b8374833d1f21c1ec341dd6112a54bf
Md Shoyeb <96253133+shoyebmd424@users.noreply.github.com>
2025-02-03 17:31:54 +0530

    added second one

---
+++ appCrud/Controllers/ClientController.cs
@@ -0,0 +1,103 @@
+﻿using appCrud.Models;
+using appCrud.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace appCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public ClientController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public List<Client> GetClients()
+        {
+            return context.Clients.OrderByDescending(c => c.Id).ToList();
+        }
+
+        [HttpGet("test")]
+        public string Sample()
+        {
+            return "Server working fine";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetClient(int id)
+        {
+            var client = context.Clients.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return Ok(client);
+        }
+
+        [HttpPost]
+        public IActionResult CreateClient(Client client)
+        {
+            try
+            {
+                var existingClient = context.Clients.FirstOrDefault(c => c.Email == client.Email);
+                if (existingClient != null)
+                {
+                    ModelState.AddModelError("Email", "The Email Address is already used");
+                    var validation = new ValidationProblemDetails(ModelState);
+                    return BadRequest(validation);
+                }
+
+                client.CreatedAt = DateTime.Now;
+                context.Clients.Add(client);
+                context.SaveChanges();
+                return Ok(client);
+            }catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error saving client: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateClient(int id, Client client)
+        {
+            var existingClient = context.Clients.FirstOrDefault(c => c.Id != id && c.Email == client.Email);
+            //if (existingClient != null)
+            //{
+            //    ModelState.AddModelError("Email", "The Email Address is already used");
+            //    var validation = new ValidationProblemDetails(ModelState);
+            //    return BadRequest(validation);
+            //}
+
+            var cnt = context.Clients.Find(id);
+            if (cnt == null)
+            {
+                return NotFound();
+            }
+
+            // Updating the client data
+            cnt.FirstName = client.FirstName;
+            cnt.Email = client.Email;
+
+            context.SaveChanges();
+            return Ok(cnt);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteClient(int id)
+        {
+            var client = context.Clients.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            context.Clients.Remove(client);
+            context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ appCrud/Migrations/20250128131936_PendingChangesMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace appCrud.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingChangesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_clients",
+                table: "clients");
+
+            migrationBuilder.RenameTable(
+                name: "clients",
+                newName: "Clients");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Clients",
+                table: "Clients",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Clients",
+                table: "Clients");
+
+            migrationBuilder.RenameTable(
+                name: "Clients",
+                newName: "clients");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_clients",
+                table: "clients",
+                column: "Id");
+        }
+    }
+}
